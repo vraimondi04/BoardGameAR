ac07de4d5a3b4213f2ad051e660ebce18253e0b3
Vincent Raimondi <vraim001@ucr.edu>
2023-09-04 20:25:58 -0700

    Added Initial Expo Camera Functionality.

--- App.js
+++ App.js
@@ -1,13 +1,47 @@
-import { StatusBar } from "expo-status-bar";
-import { Alert, Text, View, Button } from "react-native";
+import { Camera, CameraType } from 'expo-camera';
+import { useState } from 'react';
+import { Button, Text, TouchableOpacity, View, StatusBar, Alert } from 'react-native';
 
 export default function App() {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View className="flex-1 justify-center">
+        <Text className="text-center">We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  const toggleCameraType = () => {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  }
+
+  const returnToPrev = () =>  {
+    Alert.alert("Need to implement lol (separate to different screens / components)");
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>PLS</Text>
-      <Button onPress={() => Alert.alert('Simple Button Pressed.')} title='Press me'></Button>
-      <StatusBar style="auto" />
+    <View className="flex-1 justify-center">
+      <Camera className="flex-1" type={type}>
+        <View className="flex-1 flex-row bg-transparent mx-24 items-end">
+          <TouchableOpacity className="flex-1 justify-end items-center" onPress={toggleCameraType}>
+            <Text className="text-white font-bold text-xl mb-24">Flip Camera</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="flex-1 justify-end items-center" onPress={returnToPrev}>
+            <Text className="text-white font-bold text-xl mb-24">Go Back</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+      <StatusBar barStyle="dark-content"/>
     </View>
   );
 }
